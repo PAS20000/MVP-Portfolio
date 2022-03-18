f0d9fa959e4095e321c2f53f3a7433f09cda5031
PAS818 <83708869+PAS19@users.noreply.github.com>
2022-03-18 15:31:18 -0300

    deploy variáveis

--- mvp/src/utils/upload.ts
+++ mvp/src/utils/upload.ts
@@ -3,9 +3,9 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 
 aws.config.update({
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    region: process.env.AWS_REGION
+    secretAccessKey: process.env.AWSSECRET_KEY,
+    accessKeyId: process.env.AWSACCESS_KEY,
+    region: process.env.AWSREGION
 });
 
 const s3 = new aws.S3({});
@@ -13,7 +13,7 @@ const s3 = new aws.S3({});
 const upload = multer({
     storage: multerS3({
         s3,
-        bucket: process.env.AWS_BUCKET,
+        bucket: process.env.AWSBUCKET,
         acl:'public-read',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         metadata(req: any, file: { fieldname: any; }, cb: (arg0: any, arg1: { fieldName: any; }) => void) : void {
